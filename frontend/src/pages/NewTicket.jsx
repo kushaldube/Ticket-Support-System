731901a3fda071a7b075e0de7fe4ed343a2bebec
Kushal Dube <kushaldube02@gmail.com>
2023-07-11 11:52:49 +0530

    Update NewTicket.jsx

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -44,51 +44,53 @@ function NewTicket() {
 
   return (
     <>
-      <BackButton url="/" />
-      <section className="heading">
-        <h1>Create New Ticket</h1>
-        <p>Please fill out the form below</p>
-      </section>
+      <section className= "outerArea">
+        <BackButton url="/" />
+        <section className="heading">
+          <h1>Create New Ticket</h1> 
+          <p>Please fill out the form below</p>
+        </section>
 
-      <section className="form">
-        <div className="form-group">
-          <label htmlFor="name">Customer Name</label>
-          <input type="text" className="form-control" value={name} disabled />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Customer Email</label>
-          <input type="text" className="form-control" value={email} disabled />
-        </div>
-        <form onSubmit={onSubmit}>
+        <section className="form">
           <div className="form-group">
-            <label htmlFor="product">Product</label>
-            <select
-              name="product"
-              id="product"
-              value={product}
-              onChange={(e) => setProduct(e.target.value)}
-            >
-              <option value="iPhone">iPhone</option>
-              <option value="Macbook Pro">Macbook Pro</option>
-              <option value="iMac">iMac</option>
-              <option value="iPad">iPad</option>
-            </select>
+            <label htmlFor="name">Customer Name</label>
+            <input type="text" className="form-control" value={name} disabled />
           </div>
           <div className="form-group">
-            <label htmlFor="description">Description of the issue</label>
-            <textarea
-              name="description"
-              id="description"
-              className="form-control"
-              placeholder="Description"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            ></textarea>
+            <label htmlFor="email">Customer Email</label>
+            <input type="text" className="form-control" value={email} disabled />
           </div>
-          <div className="form-group">
-            <button className="btn btn-block">Submit</button>
-          </div>
-        </form>
+          <form onSubmit={onSubmit}>
+            <div className="form-group">
+              <label htmlFor="product">Issue</label>
+              <select
+                name="product"
+                id="product"
+                value={product}
+                onChange={(e) => setProduct(e.target.value)}
+              >
+                <option value="iPhone">Furniture</option>
+                <option value="Macbook Pro">Electrical</option>
+                <option value="iMac">Wifi</option>
+                <option value="iPad">Cleaning</option>
+              </select>
+            </div>
+            <div className="form-group">
+              <label htmlFor="description">Description of the issue</label>
+              <textarea
+                name="description"
+                id="description"
+                className="form-control"
+                placeholder="Description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              ></textarea>
+            </div>
+            <div className="form-group">
+              <button className="btn btn-block">Submit</button>
+            </div>
+          </form>
+        </section>
       </section>
     </>
   )
